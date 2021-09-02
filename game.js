f7f9dd73b6e038c34682c9ba4675f211500dca18
Diogo Soares Peixoto <diogosoarespeixoto41@gmail.com>
2021-09-02 15:20:53 -0300

    90% completo
    falta score e reset

---
+++ game.js
@@ -0,0 +1,56 @@
+let tabuleiro = ['', '','','', '','','', '',''];
+let vezDoJogador = 0;
+let jogadores = ['x','o'];
+let fimDeJogo = false;
+let sequenciV = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+
+function movimento(numCasa){
+    
+    if(fimDeJogo){
+        return;
+    }
+
+    if (tabuleiro[numCasa] == ''){ // se a casa estiver vazia continui
+        
+        tabuleiro[numCasa] = jogadores[vezDoJogador]; //coloque X ou O na casa escolhida de acordo com o jogador da vez
+
+        fimDeJogo = vitoria(); // ouve uma vitoria ?
+
+        if(fimDeJogo == false){  // Se não ouver um vencedor passa a vez para outro jogador
+
+            vezDoJogador = (vezDoJogador == 0) ?  1 : 0; 
+        }
+    }
+
+    return fimDeJogo;    
+}
+
+function vitoria(){ //Essa função sera executada toda vez que ouver uma jogada
+    
+    for(let i = 0; i < sequenciV.length;i++){ // repita isso 8 X
+        
+        let seq = sequenciV[i]; // Seq e igual a uma [sequencia] de vitoria
+        
+        let pos1 = seq[0]; //Pos e igual a um [numero] da sequencia de vitoria
+        let pos2 = seq[1];
+        let pos3 = seq[2];
+
+        if(tabuleiro[pos1] == tabuleiro[pos2] && tabuleiro[pos1] == tabuleiro[pos3] && tabuleiro[pos1] != ''){
+            return true;
+        }
+
+    }
+    return false;
+}
+
+
---
+++ interface.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded',()=>{ //Quando a pagina carregar execute essa função 
+
+    let casa = document.querySelectorAll(".casa"); //selecione todas a classe casa
+
+    casa.forEach((casa)=>{ //Adicione em toda a classe casa essa função
+        casa.addEventListener('click',posClick); //apos a cassa for clicada acione essa função
+    })
+
+
+})
+
+function posClick(evento){//
+    
+    let casa = evento.target; // informa a localização da casa clicada
+    let posicao = casa.id;
+
+    if(movimento(posicao)){;// Envie para a função movimento a posição da casa escolhida
+        reiniciar();
+    }
+
+    simboloCasa(posicao);
+
+}
+
+function simboloCasa(posicao){ //Essa função preenche a casa com o simbolo do jogador da vez
+    
+    let casa = document.getElementById(posicao.toString());
+    let simbolo = tabuleiro[posicao];
+    casa.innerHTML = `<div class='${simbolo}'></div>`
+        
+    
+    
+}
+
